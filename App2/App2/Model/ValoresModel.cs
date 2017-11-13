10c744c368b65039177d101a85584cd27ec11808
Pedro Jesus <pedrojesus.cefet@gmail.com>
2017-11-13 13:48:35 -0200

    Adicionar arquivos de projeto.

---
+++ App2/App2/Model/ValoresModel.cs
@@ -0,0 +1,18 @@
+﻿namespace App2.Model
+{
+    public class ValoresModel
+    {
+        public string Quantidade { get; set; }
+
+        public string Preco { get; set; }
+
+        public string Resultado { get; set; }
+
+        public ValoresModel(string quantidade, string preco, string resultado)
+        {
+            Quantidade = quantidade;
+            Preco = preco;
+            Resultado = resultado;
+        }
+    }
+}
---
+++ App2/App2/ViewModel/PrincipalViewModel.cs
@@ -0,0 +1,84 @@
+﻿using App2.Model;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace App2.ViewModel
+{
+    public class PrincipalViewModel : BaseViewModel
+   {
+        #region Propriedade
+
+        CultureInfo culture;
+
+        private string _quant;
+
+        public string Quant
+        {
+            get { return _quant; }
+            set { _quant = value; OnPropertyChanged(); }
+        }
+
+        private string _valor;
+
+        public string Valor
+        {
+            get { return _valor; }
+            set { _valor = value; OnPropertyChanged(); }
+        }
+
+        private string _result;
+
+        public string Result
+        {
+            get { return _result; }
+            set { _result = value; OnPropertyChanged(); }
+        }
+
+        private string _minimo;
+
+        public string Minimo
+        {
+            get { return _minimo; }
+            set { _minimo = value; OnPropertyChanged(); }
+        }
+
+        #endregion
+        #region Command
+        public ObservableCollection<ValoresModel> Valores { get; set; }
+
+        public Command CalcularCommand { get; }
+        
+        public Command LimparCommand { get; }
+#endregion
+        public PrincipalViewModel()
+        {
+            Valores = new ObservableCollection<ValoresModel>();
+            CalcularCommand = new Command(ExecuteCalcularCommand);
+            LimparCommand = new Command(ExecuteLimparCommand);
+            culture = new CultureInfo("pt-BR");
+        }
+
+        private void ExecuteCalcularCommand()
+        {
+            float resultado = (float.Parse(Valor,culture) * 1000) / float.Parse(Quant,culture);
+            Result =resultado.ToString("N2",culture);
+            Valores.Add(new ValoresModel(Quant, Valor, Result));
+            float valorMinimo = 9999999;
+            
+            if (valorMinimo > resultado)
+            {
+                Minimo = $"Preço de tabela: R${Valor}, Preço calculado:R${Result}";
+                valorMinimo = resultado;
+            }
+        }
+
+        private void ExecuteLimparCommand()
+        {
+            Valores.Clear();
+            Result = "";
+            Valor = "";
+            Quant = "";
+        }
+    }
+}
